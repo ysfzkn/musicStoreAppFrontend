02c7ae86281c894322b53580373d72382969d890
Yusuf Özkan <58569590+ysfzkn@users.noreply.github.com>
2022-06-17 16:14:02 +0200

    envireonment for production

---
+++ set-env.js
@@ -0,0 +1,15 @@
+const{writeFile} = require('fs');
+
+require('dotenv').config();
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const targetPath = isProduction ? `./src/environments/environtment.prod.ts` :
+ `./src/environments/environtment.ts` ;
+
+ const envFileContent = `
+ export const environment = {
+    production: ${isProduction},
+    BASE_URL: "${process.env.BASE_URL}"
+  };
+ `;
