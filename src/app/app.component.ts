1014a2b958c129e02e56fcd5a2a02587d1c1f21d
Yusuf Özkan <58569590+ysfzkn@users.noreply.github.com>
2022-06-20 02:01:47 +0200

    navbar ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,11 @@ import { Router } from '@angular/router';
 import { Role } from './model/role.enum';
 import { User } from './model/user.model';
 import { AuthService } from './service/auth.service';
+import {faShoppingCart, faSignIn, faUserPlus, 
+        faHome, faLock, faUser, faSignOut} from "@fortawesome/free-solid-svg-icons";
+import {PurchaseItem} from "./model/purchase-item.model";
+import {PurchaseService} from "./service/purchase.service";
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,12 +16,25 @@ import { AuthService } from './service/auth.service';
 })
 export class AppComponent 
 {
+  faSignout = faSignOut;
+  faUser = faUser;
+  faShop = faShoppingCart;
+  faRegister =faUserPlus;
+  faLogin = faSignIn;
+  faHome = faHome;
+  faAdmin = faLock;
   title = 'musicStoreAppFrontend';
-
+  purchaseItemList: Array<PurchaseItem> = [];
   currentUser: User = new User;
+  currentCount = new BehaviorSubject(0);
 
-  constructor(private authenticationService: AuthService, private router: Router) 
+  constructor(private authenticationService: AuthService, 
+    private router: Router,private purchaseService: PurchaseService) 
   {
+    this.purchaseService.getAllPurchaseItems().subscribe(data => {
+      this.purchaseItemList = data;
+      this.currentCount = data.length;
+    });
     this.authenticationService.currentUser.subscribe(data => {
       this.currentUser = data;
     });
