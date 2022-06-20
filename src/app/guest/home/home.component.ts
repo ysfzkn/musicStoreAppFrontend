10493a01bd506de461b63e3c3ca2413b70e1acd0
Yusuf Özkan <58569590+ysfzkn@users.noreply.github.com>
2022-06-20 02:02:10 +0200

    add item image

--- src/app/guest/home/home.component.ts
+++ src/app/guest/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Instrument} from "../../model/instrument.model";
-import {faGuitar} from "@fortawesome/free-solid-svg-icons";
+import {faGuitar , faPlus, faShoppingCart} from "@fortawesome/free-solid-svg-icons";
 import {AuthService} from "../../service/auth.service";
 import {InstrumentService} from "../../service/instrument.service";
-import {PurchaseService} from "../../service/purchase.service";
 import {Purchase} from "../../model/purchase.model";
+import {PurchaseService} from "../../service/purchase.service";
+import { PurchaseItem } from 'src/app/model/purchase-item.model';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-home',
@@ -15,17 +17,25 @@ export class HomeComponent implements OnInit {
 
   instrumentList: Array<Instrument> = [];
   faGuitar = faGuitar;
+  faPlus= faPlus;
+  faShop= faShoppingCart;
   errorMessage: string = "";
   infoMessage: string = "";
+  purchaseItemList: Array<PurchaseItem> = [];
 
   constructor(private authService: AuthService,
               private instrumentService: InstrumentService,
-              private purchaseService: PurchaseService) { }
+              private purchaseService: PurchaseService,
+              private appComp : AppComponent) { }
 
   ngOnInit(): void {
+    
     this.instrumentService.getAllInstruments().subscribe(data => {
       this.instrumentList = data;
     })
+    this.purchaseService.getAllPurchaseItems().subscribe(data => {
+      this.purchaseItemList = data;
+    });
   }
 
   purchase(item: Instrument) {
@@ -36,8 +46,13 @@ export class HomeComponent implements OnInit {
 
     const purchase = new Purchase(this.authService.currentUserValue.id, item.id, item.price);
 
-    this.purchaseService.savePurchase(purchase).subscribe(data => {
-      this.infoMessage = 'Mission is completed';
+    this.purchaseService.savePurchase(purchase).subscribe(data => 
+    {
+      this.purchaseService.getAllPurchaseItems().subscribe(res => {
+        this.purchaseItemList = res;
+        this.appComp.currentCount = res.length;
+      });
+      this.infoMessage = 'An Instrument added to card successfully';
     }, err => {
       this.errorMessage = 'Unexpected error occurred.';
       console.log(err);
